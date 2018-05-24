cb2ac8fdebe4f6b34f5209e8c165b05530b1dd96
GorbunovaAA <gorbunova.aa@phystech.edu>
2018-05-24 13:57:59 +0300

    Добавлено поддержания нескольких чатов

--- bot.py
+++ bot.py
@@ -2,18 +2,15 @@ import telebot
 import bot_commands
 import re
 
-commands_list = ['start', 'help']
 
-
-# Число выводимых последних тем/статей по умолчанию
-command_list = ['start', 'help', 'new_user',
+COMMAND_LIST = ['start', 'help', 'new_user',
                 'all_users', 'new_debt', 'all_debts', 'all_undebts']
 # Список команд, принимаемых ботом
-greeting = '''Приветствую! Я бот, который поможет Вам\n
+GREETING = '''Приветствую! Я бот, который поможет Вам\n
             следить за задолженностями между Вами и вашими друзьями.\n
             Чтобы прочесть список команд, введите /help'''
 # Приветствие при начале использования бота
-help_list = '''/help - список команд\n
+HELP_LIST = '''/help - список команд\n
 /start - начало общения с ботом\n
 /new_user - добавление нового участника\n
 Принимаемые параметры:\nИмя, Фамилия\n
@@ -31,6 +28,9 @@ help_list = '''/help - список команд\n
 '''
 
 
+cur_chat_id = 0
+
+
 with open('TOKEN.txt', 'r') as file:
     TOKEN = file.read()
 bot = telebot.TeleBot(TOKEN) 
@@ -44,38 +44,49 @@ class User:
 
 @bot.message_handler(command='new_user')
 def new_user(message):
-    result = bot_commands.add_user(message.text)
+    bot_commands.check_chat_id(message.chat.id)
+    chat_id = message.chat.id
+    result = bot_commands.add_user(message.text, chat_id)
     bot.send_message(message.chat.id, result)
 
 
 @bot.message_handler(command='all_users')
 def all_users(message):
+    bot_commands.check_chat_id(message.chat.id)
     result = bot_commands.all_users()
     bot.send_message(message.chat.id, result)
 
 
 @bot.message_handler(command='new_debt')
 def new_debt(message):
+    bot_commands.check_chat_id(message.chat.id)
     result = bot_commands.update_debt(message.text)
     bot.send_message(message.chat.id, result)
 
 
 @bot.message_handler(command='all_debts')
 def get_all_debts(message):
+    bot_commands.check_chat_id(message.chat.id)
     result = bot_commands.get_all_debts(message.text)
     bot.send_message(message.chat.id, result)
 
 
 @bot.message_handler(command='all_undebts')
 def get_all_undebts(message):
+    bot_commands.check_chat_id(message.chat.id)
     result = bot_commands.get_all_undebts(message.text)
     bot.send_message(message.chat.id, result)
 
 
 @bot.message_handler(command='ask_debt')
 def get_all_undebts(message):
+    bot_commands.check_chat_id(message.chat.id)
     result = bot_commands.ask_debt(message.text)
     bot.send_message(message.chat.id, result)
 
 if __name__ == '__main__':
-    bot.polling(none_stop=True)
+    while True:
+        try:
+            bot.polling(none_stop=True)
+        except RuntimeError:
+            pass
